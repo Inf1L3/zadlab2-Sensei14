350e9763328e57f862c9922e72fddbed3bb59e37
student <student@pc-108-21>
2018-03-07 18:00:59 +0100

    Lab

--- POLab2/src/pl/edu/ur/main/Main.java
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -4,6 +4,8 @@ package pl.edu.ur.main;
  *
  * @author marcin
  */
+import java.util.Scanner;
+
 public class Main {
 
     /**
@@ -11,6 +13,129 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO zad 3, 4, 6, 7
+
+        int x;
+
+        Scanner odczyt = new Scanner(System.in);
+        /*  System.out.println("Podaj miesiąc: (Żeby przerwać wpisz 0)");
+        x = odczyt.nextInt();
+        
+            switch (x) {
+                case 1:
+                    System.out.println("Styczeń");
+                    break;
+
+                case 2:
+                    System.out.println("Luty");
+                    break;
+
+                case 3:
+                    System.out.println("Marzec");
+                    break;
+
+                case 4:
+                    System.out.println("Kwiecień");
+                    break;
+
+                case 5:
+                    System.out.println("Maj");
+                    break;
+
+                case 6:
+                    System.out.println("Czerwiec");
+                    break;
+
+                case 7:
+                    System.out.println("Lipiec");
+                    break;
+
+                case 8:
+                    System.out.println("Sierpień");
+                    break;
+
+                case 9:
+                    System.out.println("Wrzesień");
+                    break;
+
+                case 10:
+                    System.out.println("Październik");
+                    break;
+
+                case 11:
+                    System.out.println("Listopad");
+                    break;
+
+                case 12:
+                    System.out.println("Grudzień");
+                    break;
+
+                default:
+                    System.out.println("Podałeś zły miesiąc.");
+                    break;
+            }
+        }
+         
+        int i = 0;
+        int a = 0;
+        int skok = 100;
+        System.out.println("DO WHILE");
+        do {
+
+            a += skok;
+
+            i++;
+            System.out.println("Iteracja:" + i);
+            System.out.println("a=" + a);
+        } while (a < 1000);
+        
+        
+        a=0;
+        i=0;
+        System.out.println("WHILE");
+        while(a<1000)
+        {
+            a += skok;
+
+            i++;
+            System.out.println("Iteracja:" + i);
+            System.out.println("a=" + a);
+        }
+       
+
+        
+        for(int i=20; i>=0; i--)
+        {
+            if(i==2 || i==6 || i==9 || i==15 || i==19 ) continue;
+            else
+                System.out.println(i);
+            
+        }
+         */
+        
+        
+        int a,b,c;
+        double x1, x2, delta;
+        
+        a=1; b=2; c=1;
+        
+        delta=b*b-4*a*c;
+        
+        if(delta<0) System.out.println("Brak rozwiązań, delta ujemna");
+        else
+            if(delta==0)
+            {
+                x1= -b/2*a;
+                System.out.println("Delta=0, x0= "+x1);
+            }
+        else
+            {
+                x1= (-b - Math.sqrt(delta))/2*a;
+                x2= (-b + Math.sqrt(delta))/2*a;
+                System.out.println("delta="+delta+" x1="+x1+" x2="+x2);
+            }
+        
+        
+        
     }
-    
+
 }
